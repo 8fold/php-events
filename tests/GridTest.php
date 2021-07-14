6b0f7164b2ed402343ee3c12e7b2e4d225bc23da
Josh Bruce <josh@8fold.pro>
2021-07-14 13:55:15 -0500

    perofrmance

--- tests/GridTest.php
+++ tests/GridTest.php
@@ -205,7 +205,7 @@ class GridTest extends TestCase
         AssertEquals::applyWith(
             '<button id="toggle-20200522" class="calendar-date" onclick="EFEventsModals.init(this, 20200522)" aria-expanded="false"><abbr title="22nd of May 2020">22</abbr><span>Hello, Event!</span><span>Hello, Day?</span></button>',
             "string",
-            35.11, // 25.5, // 24.31, // 23.61,
+            35.36, // 35.11, // 25.5, // 24.31, // 23.61,
             2994
         )->unfoldUsing(
             Grid::forMonth($this->path->unfold(), 2020, 5)->gridItem(22)
