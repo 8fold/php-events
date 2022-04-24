c5f221322c0041fd0971f1ebdb19e1c58c08c87c
Josh Bruce <josh@8fold.pro>
2022-04-24 15:23:57 -0400

    deprecate: pest - GridYearBaselineTest

--- tests/UI/GridYearBaselineTest.php
+++ tests/UI/GridYearBaselineTest.php
@@ -1,46 +1,68 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Eightfold\Events\Tests\UI;
+
+use PHPUnit\Framework\TestCase;
 
 use Eightfold\Events\UI\GridForYear;
 
 use Eightfold\FileSystem\Item;
 
-// beforeEach(function() {
-//     $this->path = Item::create(__DIR__)
-//         ->up()->append('test-events', 'events')->thePath();
-// });
-//
-// test('Year grid has next and previous links', function() {
-//     expect(
-//         GridForYear::fold($this->path, 2020)->previousLink()->build()
-//     )->toBe(
-//         '<span class="ef-grid-previous-year"></span>'
-//     );
-//
-//     expect(
-//         GridForYear::fold($this->path, 2021)->previousLink()->build()
-//     )->toBe(
-//         '<a class="ef-grid-previous-year" href="/events/2020" title="2020"><span>2020</span></a>'
-//     );
-//
-//     expect(
-//         GridForYear::fold($this->path, 2021)->nextLink()->build()
-//     )->toBe(
-//         '<a class="ef-grid-next-year" href="/events/2022" title="2022"><span>2022</span></a>'
-//     );
-//
-//     expect(
-//         '<a href="/events/2020/05"><abbr title="May 2020">May</abbr><span>5</span></a>'
-//     )->toBe(
-//         GridForYear::fold($this->path, 2020)->gridItem(5)->build()
-//     );
-// })->group('ui', 'year');
-//
-// test('Year grid has expected title', function() {
-//     expect(
-//         GridForYear::fold($this->path, 2020)->header()->build()
-//     )->toBe(
-//         '<h2>2020</h2>'
-//     );
-// })->group('ui', 'year');
+class GridYearBaselineTest extends TestCase
+{
+    private string $path = '';
+
+    public function setUp(): void
+    {
+        $this->path = Item::create(__DIR__)->append('test-events', 'events')
+            ->thePath();
+    }
+
+    /**
+     * @test
+     *
+     * @group ui
+     * @group year
+     */
+    public function year_grid_has_next_and_previous_links(): void
+    {
+        $this->assertEquals(
+            '<span class="ef-grid-previous-year"></span>',
+            GridForYear::fold($this->path, 2020)->previousLink()->build()
+        );
+
+        // TODO: assertion is broken
+        // $this->assertEquals(
+        //     '<a class="ef-grid-previous-year" href="/events/2020" title="2020"><span>2020</span></a>',
+        //     GridForYear::fold($this->path, 2021)->previousLink()->build()
+        // );
+
+        // TODO: assertion is broken
+        // $this->assertEquals(
+        //     '<a class="ef-grid-next-year" href="/events/2022" title="2022"><span>2022</span></a>',
+        //     GridForYear::fold($this->path, 2021)->nextLink()->build()
+        // );
+
+        // TODO: assertion is broken
+        // $this->assertEquals(
+        //     '<a href="/events/2020/05"><abbr title="May 2020">May</abbr><span>5</span></a>',
+        //     GridForYear::fold($this->path, 2020)->gridItem(5)->build()
+        // );
+    }
+
+    /**
+     * @test
+     *
+     * @group ui
+     * @group year
+     */
+    public function year_grid_has_expected_title(): void
+    {
+        $this->assertEquals(
+            '<h2>2020</h2>',
+            GridForYear::fold($this->path, 2020)->header()->build()
+        );
+    }
+}
